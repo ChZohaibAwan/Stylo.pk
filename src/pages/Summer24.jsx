206e34d7c06e4fee47fbd18740cdc12b33bb43e1
chzohaibawan <zohaib.vconttech@gmail.com>
2024-08-11 08:16:08 -0700

    summer products api

--- src/pages/Summer24.jsx
+++ src/pages/Summer24.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect,useState} from 'react'
 
 const Summer24 = () => {
+  const[products,setProducts]=useState([])
+  const fetchdata=async()=>{
+    const respoonse=await axios.get ("https://fakestoreapi.com/products/category/jewelery");
+    setProducts(respoonse.data)
+  }
+useEffect(()=>{
+  fetchdata()
+  
+})
   return (
-    <div>
-      Summer24
+    <div className='gewelery'>
+      {products.map((product)=>{
+        return(<div className="card" style={{width: "18rem"}}>
+          <img src={product.image} style={{width: "100px", height: "100px"}} class="card-img-top" alt="..."/>
+          <div class="card-body" src={product.image}>
+            <h5 class="product.title" >src={product.title}</h5>
+            <p class="description" >src={product.description}</p>
+            <a href="#" class="btn btn-primary">src={product.price}</a>
+          </div>
+        </div>)
+      })}
     </div>
   )
 }
